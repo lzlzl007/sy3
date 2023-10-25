52df9fd4168b4ed24c25299c6ef52a86f87e59ef
“lzlzl007“ <lipengyu1669031574@gmail.com>
2023-10-25 10:49:34 +0800

    jianfa

--- include/util.h
+++ include/util.h
@@ -2,5 +2,5 @@
 #define UTIL_H
 
 int add(int a, int b);
-
+int jian(int a, int b);
 #endif
--- src/util.c
+++ src/util.c
@@ -3,3 +3,6 @@
 int add(int a, int b) {
     return a + b;
 }
+int jian(int a, int b) {
+    return a - b;
+}
