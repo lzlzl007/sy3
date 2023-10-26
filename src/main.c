9428b95d894c39e6426f3fa4e350c1c2b4dadb02
lzlzl007 <lipengyu1669031574@gmail.com>
2023-10-26 14:03:42 +0800

    从这里开始是第三次实验的内容

--- src/main.c
+++ src/main.c
@@ -2,9 +2,9 @@
 #include "util.h"
 #include "float_addition.h"
 int main() {
-    float a=5.0;
+    float a=5.8;
     float b=3.2;
     int result = add_float(a,b);
-    printf("The result is: %d\n", result);
+    printf("The result is: %d\n", result);//从这里开始是实验三的部分
     return 0;
 }
